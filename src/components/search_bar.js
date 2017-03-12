d1508634ccc94b54d03fd9fa68f164de84b8317a
David Wybourn <david.wybourn@gmail.com>
2017-03-12 17:44:51 +0000

    Section 1 completed. Added simple search bar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {'searchTerm': 'Starting Value'};
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.searchTerm}
+                    onChange={ event => this.setState({'searchTerm': event.target.value}) }/>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from "react-dom";
+import React from "react";
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDv9O-OdVxEKpqJ9TPabDQD3awNCNQntis';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
+
+ReactDom.render(<App/>, document.querySelector('.container'));
